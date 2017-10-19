864f1d4644918ddd09596a1e765730d6071644ed
lwom456 <393953485@qq.com>
2017-10-19 22:11:50 +0800

    进度2

--- Java_homework/src/fourth_class/InPutData.java
+++ Java_homework/src/fourth_class/InPutData.java
@@ -1,5 +1,20 @@
 package fourth_class;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //从文件中读取数据
 public class InPutData {
-
+	public static int[][] inputData(String name,int n) throws FileNotFoundException{
+		Scanner sc = new Scanner(new FileInputStream(name));
+		int[][] data = new int[n][3];
+		for(int i=0;i<n;i++) {
+			data[i][0]=sc.nextInt();
+			data[i][1]=sc.nextInt();
+			data[i][2]=sc.nextInt();
+		}
+		sc.close();
+		return data;
+	}	
 }
--- Java_homework/src/fourth_class/Test.java
+++ Java_homework/src/fourth_class/Test.java
@@ -1,13 +1,21 @@
 package fourth_class;
 
+import java.io.IOException;
+
 //≤‚ ‘≥Ã–Ú
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Task t = new Task(0,0,6);
-		t.startService(4);
-		System.out.println(t);
+		int N =100;
+		MakeTestFile.makeTestFile(N);
+		int[][] data=InPutData.inputData("testData.txt",N);
+		DoubleFCFS sFCFS = new DoubleFCFS(data);
+		sFCFS.startService();
+		Task[] task=sFCFS.getTask();
+		for(int i=0;i<task.length;i++) {
+			System.out.println(task[i]);
+		}
 	}
 
 }
