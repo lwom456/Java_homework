c091bf24fb58352427db838377762f9b57ed00a1
lwom456 <393953485@qq.com>
2017-11-18 19:00:45 +0800

    sortStuData

--- Java_homework/src/java_io_stream/ObjectSerialization.java
+++ Java_homework/src/java_io_stream/ObjectSerialization.java
@@ -3,22 +3,59 @@ package java_io_stream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ObjectSerialization {
-
+	private LinkedList<Student> stus;
+	
+	public ObjectSerialization() {
+		stus = new LinkedList<Student>();
+	}
+	
+	public void add(long id,String name,String sex) {
+		stus.add(new Student(id,name,sex));
+	}
+	
+	public void sort() {
+		Collections.sort(stus);
+	}
+	
+	public void print() {
+		for(Student stu:stus) {
+			System.out.println(stu);
+		}
+	}
+	
+	public LinkedList<Student> getLink(){
+		return stus;
+	}
+	
 	public static void main(String[] args){
 		try {
 			File fin = new File("list.txt");
 			FileInputStream fins = new FileInputStream(fin);
 			Scanner sc;
 			sc = new Scanner(fins);
+			
+			//instantiation a ObjectSerialization Object
+			ObjectSerialization os = new ObjectSerialization();
 			while(sc.hasNext()) {
-				System.out.print(sc.nextLong()+"  ");
-				System.out.print(sc.next()+"      ");
-				System.out.println(sc.next());
+				//input data
+				os.add(sc.nextLong(),sc.next(),sc.next());
 			}
 			sc.close();
+			os.sort();
+			System.out.println("排序后输出：");
+			os.print();
+			
+			//save Object
+			File fout = new File("student.bin");
+			//ObjectOutputStream 
+			//read OBject
+			fin = new File("student.bin");
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Java_homework/src/java_io_stream/Student.java
+++ Java_homework/src/java_io_stream/Student.java
@@ -1,5 +1,23 @@
 package java_io_stream;
 
-public class Student {
-
+public class Student implements Comparable<Student>{
+	long id;
+	String name;
+	String sex;
+	public Student(long id,String name,String sex) {
+		this.id=id;
+		this.name=name;
+		this.sex=sex;
+	}
+	@Override
+	public int compareTo(Student stu) {
+		int result = id > stu.id ? 1 : (id == stu.id ? 0 : -1);
+		return result;
+	}
+	@Override
+	public String toString() {
+		return id + "  " + name + "     " + sex;
+	}
+	
+	
 }
